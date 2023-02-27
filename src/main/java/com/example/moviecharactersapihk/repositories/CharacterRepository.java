91b8aea20d5133e4cbcf8c4f974b19ec223158ad
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-27 10:39:01 +0100

    feat: seeding data into data base

--- src/main/java/com/example/moviecharactersapihk/repositories/CharacterRepository.java
+++ src/main/java/com/example/moviecharactersapihk/repositories/CharacterRepository.java
@@ -1,2 +1,23 @@
+<<<<<<< Updated upstream
 package com.example.moviecharactersapihk.repositories;public class CharacterRepository {
 }
+=======
+package com.example.moviecharactersapihk.repositories;
+import com.example.moviecharactersapihk.models.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
+    @Query(value = "select distinct * from character " +
+            "inner join movie_character  on character.id = movie_character.character_id  " +
+            "inner join movie on movie_character.movie_id=movie.id " +
+            " inner join franchise on franchise.id=movie.franchise_id " +
+            "where franchise.id = ?1 ", nativeQuery = true)
+    Optional<Set<Character> >findAllCharactersByFranchise(int franchiseId);
+}
+>>>>>>> Stashed changes
