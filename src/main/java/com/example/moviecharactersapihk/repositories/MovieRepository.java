f1fbab98f7c156fb6a428395d074f701f9486e3c
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-03-01 16:02:50 +0100

    Delete MovieRepository.java

--- src/main/java/com/example/moviecharactersapihk/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.moviecharactersapihk.repositories;
-
-import com.example.moviecharactersapihk.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
